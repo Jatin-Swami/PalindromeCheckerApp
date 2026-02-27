995c53acd34330e1aadb2a438929cc4c0dd5fed7
Jatin-Swami <jatinswami890@gmail.com>
2026-02-27 13:10:34 +0530

    Jatin Swami added UC-11

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,20 +1,28 @@
 import java.util.*;
-public class PalindromeCheckerApp {
-    static boolean isPalindrome(String str, int start, int end) {
-        if(start >= end) {
-            return true;
+class PalindromeChecker {
+    String str;
+    public boolean checkPalindrome() {
+        int start = 0;
+        int end = str.length() - 1;
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
         }
-        if(str.charAt(start) == str.charAt(end) || str.charAt(start) == Character.toUpperCase(str.charAt(end)) || str.charAt(start) == Character.toLowerCase(str.charAt(end))) {
-            return isPalindrome(str, ++start, --end);
-        }
-        return false;
+        return true;
     }
+    PalindromeChecker(String str) {
+        this.str = str;
+    }
+}
+public class PalindromeCheckerApp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Input text: ");
         String text = sc.nextLine();
-        text = text.trim();
-        int end = text.length();
-        System.out.println("Is it a Palindrome? " + isPalindrome(text, 0, end - 1));
+        PalindromeChecker object = new PalindromeChecker(text);
+        System.out.println("Is it a Palindrome? " + object.checkPalindrome());
     }
 }
