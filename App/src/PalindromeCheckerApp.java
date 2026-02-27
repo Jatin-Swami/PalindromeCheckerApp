4e46bf86aa786f16696f84081528c65968a47747
Jatin-Swami <jatinswami890@gmail.com>
2026-02-27 11:26:21 +0530

    Jatin Swami added UC-10

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -4,15 +4,17 @@ public class PalindromeCheckerApp {
         if(start >= end) {
             return true;
         }
-        if(str.charAt(start) != str.charAt(end)) {
-            return false;
+        if(str.charAt(start) == str.charAt(end) || str.charAt(start) == Character.toUpperCase(str.charAt(end)) || str.charAt(start) == Character.toLowerCase(str.charAt(end))) {
+            return isPalindrome(str, ++start, --end);
         }
-        return isPalindrome(str, ++start, --end);
+        return false;
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Input text: ");
         String text = sc.nextLine();
-        System.out.println("Is it a Palindrome? " + isPalindrome(text, 0, text.length() -1 ));
+        text = text.trim();
+        int end = text.length();
+        System.out.println("Is it a Palindrome? " + isPalindrome(text, 0, end - 1));
     }
 }
